ab3b9f688eb9b8d06fbb375ca61439e015fa1dd5
pakkiraiah <pakkiraiah@gmail.com>
2017-04-20 14:36:18 +0530

    Update Demo.scala

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -4,13 +4,19 @@ import org.apache.spark.SparkContext
 object WordCount {
   def main(args: Array[String]) = {
 
+      execute();
+    
+  }
+  
+  def execute()
+  {
     val conf = new SparkConf().setAppName("WordCount").setMaster("local")
     val sc = new SparkContext(conf)
     println("reading  file")
     val test = sc.textFile("input.txt")
     println("reading file done")
 
-    test.flatMap { line => line.split(" ") }.map { word => (word, 1) }.reduceByKey(_ + _).saveAsTextFile("output.txt")
+    test.flatMap { line => line.split(" ") }.map { word => (word, 1) }.reduceByKey(_ + _).saveAsTextFile("output")
     println("writting file dones file")
 
     sc.stop
